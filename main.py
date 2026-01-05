138795202fd7749d8a56c2772c176540c1f0aad7
timothesto98 <timothesto98@gmail.com>
2026-01-05 10:37:42 -0800

    Update main.py
    Add create-user endpoint

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import os
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import declarative_base, sessionmaker
@@ -23,5 +23,25 @@ Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def root():
-    return {"status": "Database imeunganishwa na table ya users imetengenezwa"}
+    return {"status": "PJFinance API iko live"}
 
+@app.post("/create-user")
+def create_user(username: str, password: str):
+    db = SessionLocal()
+
+    existing_user = db.query(User).filter(User.username == username).first()
+    if existing_user:
+        db.close()
+        raise HTTPException(status_code=400, detail="Username tayari ipo")
+
+    user = User(username=username, password=password)
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    db.close()
+
+    return {
+        "message": "User ameundwa kikamilifu",
+        "user_id": user.id,
+        "username": user.username
+    }
